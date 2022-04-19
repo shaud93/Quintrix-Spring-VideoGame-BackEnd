f570c19e9cef8582ee733ed6cbd4ac63e217af05
shaud93 <85545994+shaud93@users.noreply.github.com>
2022-04-19 03:17:34 -0400

    Add files via upload
    added api request

---
+++ quintrix-spring/src/main/java/com/quintrix/jfs/quintrixspring/Cast.java
@@ -0,0 +1,21 @@
+package com.quintrix.jfs.quintrixspring;
+
+public class Cast {
+	private final int id;
+	private final String actor;
+	
+	public Cast(int id, String actor) {
+		this.id = id;
+		this.actor = actor;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public String getActor() {
+		return actor;
+	}
+
+}
